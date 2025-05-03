8619eb0eb3e841a879fd12327e5e2089f5383adf
Clive-MM <maderumoyia@gmail.com>
2025-05-03 15:47:40 +0300

    Frontend Setup

---
+++ mufate-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-50">
+      <h1 className="text-3xl font-bold text-green-700">
+        Welcome to MUFATE G SACCO Website
+      </h1>
+    </div>
+  )
+}
+
+export default App
