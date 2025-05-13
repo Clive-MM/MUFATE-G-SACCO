fb64ddcdace0ce34a6e0bb2bc5d68c88a2a34123
Clive-MM <maderumoyia@gmail.com>
2025-05-14 00:17:07 +0300

    Homepage sections done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Box, Typography, IconButton, Link } from '@mui/material';
+import {
+  Box, Typography, IconButton, Link
+} from '@mui/material';
 import {
   LocationOn, Email, AccessTime, Phone, ArrowUpward,
   Facebook, Instagram, X
@@ -14,9 +16,13 @@ const Footer = () => {
   return (
     <Box className="footer-container">
       <Box className="footer-content">
-       
+        {/* Column 1: Logo + Description + Socials */}
         <Box className="footer-column">
-          <img src="https://res.cloudinary.com/djydkcx01/image/upload/v1746061572/Mufate_Logo_jnnh7x.png" alt="Mufate Logo" className="footer-logo" />
+          <img
+            src="https://res.cloudinary.com/djydkcx01/image/upload/v1746061572/Mufate_Logo_jnnh7x.png"
+            alt="Mufate Logo"
+            className="footer-logo"
+          />
           <Typography className="footer-title">INVEST HERE REAP HERE</Typography>
           <Typography className="footer-description">
             Mufate ‘G’ Sacco society limited is a Sacco that started in 1987 and provides financial
@@ -29,12 +35,12 @@ const Footer = () => {
           </Box>
         </Box>
 
-       
+        {/* Column 2: Recent Posts */}
         <Box className="footer-column">
           <Typography className="footer-heading">RECENT POSTS</Typography>
           <Box className="footer-images">
             <img src="https://res.cloudinary.com/djydkcx01/image/upload/v1746217592/CROP_ADVANCE_LOAN_IMAGE_rxkpxb.png" alt="Post 1" />
-            <img src="https://res.cloudinary.com/djydkcx01/image/upload/v1746220668/school_fees_Loan_image_sslrcp.png" alt="Products Review" />
+            <img src="https://res.cloudinary.com/djydkcx01/image/upload/v1746220668/school_fees_Loan_image_sslrcp.png" alt="Post 2" />
           </Box>
           <Box className="footer-images">
             <img src="https://res.cloudinary.com/djydkcx01/image/upload/v1746215520/Schools_Sacco_Client_njawjp.jpg" alt="Post 3" />
@@ -42,7 +48,7 @@ const Footer = () => {
           </Box>
         </Box>
 
-       
+        {/* Column 3: Quick Links */}
         <Box className="footer-column">
           <Typography className="footer-heading">QUICK LINKS</Typography>
           <ul className="footer-links">
@@ -57,20 +63,26 @@ const Footer = () => {
           </ul>
         </Box>
 
-        {/* Contact Info */}
+        {/* Column 4: Contact Info */}
         <Box className="footer-column">
           <Typography className="footer-heading">CONTACT US</Typography>
-          <Box className="footer-contact">
-            <Phone /><span>+254791331932 / +254794515407</span>
-          </Box>
-          <Box className="footer-contact">
-            <LocationOn /><span>Khayega - Kakamega</span>
-          </Box>
-          <Box className="footer-contact">
-            <Email /><span>Info@mudetesacco.co.ke</span>
-          </Box>
-          <Box className="footer-contact">
-            <AccessTime /><span>Monday - Friday 8:30 - 5.30PM</span>
+          <Box className="contact-info">
+            <Box className="contact-item">
+              <Box className="icon-wrapper"><Phone /></Box>
+              <span>+254791331932 / +254794515407</span>
+            </Box>
+            <Box className="contact-item">
+              <Box className="icon-wrapper"><LocationOn /></Box>
+              <span>Khayega - Kakamega</span>
+            </Box>
+            <Box className="contact-item">
+              <Box className="icon-wrapper"><Email /></Box>
+              <span>Info@mudetesacco.co.ke</span>
+            </Box>
+            <Box className="contact-item">
+              <Box className="icon-wrapper"><AccessTime /></Box>
+              <span>Monday - Friday 8:30 - 5.30PM</span>
+            </Box>
           </Box>
         </Box>
       </Box>
@@ -80,6 +92,7 @@ const Footer = () => {
         <ArrowUpward />
       </IconButton>
 
+      {/* Footer Bottom */}
       <Box className="footer-bottom">
         <Typography variant="body2">
           Mufate Sacco Society Limited © 2024. All Rights Reserved.
