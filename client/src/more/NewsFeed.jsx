ea003772f1526ce2a7c775debbb9019516e00982
Clive Moyia <maderumoyia@gmail.com>
2026-01-14 12:05:44 +0300

    NEWSFEED

--- client/src/more/NewsFeed.jsx
+++ client/src/more/NewsFeed.jsx
@@ -111,7 +111,7 @@ const NewsFeed = () => {
     <CardMedia
         component="img"
         /* 1. Increased height from 160 to 220 or 240 for a more vertical/prominent look */
-        height="320" 
+        height="370" 
         image={post.CoverImage || 'https://via.placeholder.com/320x140'}
         sx={{ 
             /* 2. Changed 'contain' to 'cover' to fill the entire space */
