cefdfcc75ad17a4d5392190518be88c1a2bab381
Clive Moyia <maderumoyia@gmail.com>
2026-01-14 11:45:24 +0300

    NEWSFEED

--- client/src/more/NewsFeed.jsx
+++ client/src/more/NewsFeed.jsx
@@ -41,7 +41,7 @@ const NewsFeed = () => {
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
-        return `${day}${month}${year}`;
+        return `${day}/${month}/${year}`;
     };
 
     const handleSubscribe = async () => {
