e2f2e6029c7d2b1f3e378ddfd096cce1cf4a104d
Clive Moyia <maderumoyia@gmail.com>
2026-01-24 13:14:14 +0300

    Background color changed

--- client/src/pages/Products/LoanCalculator.jsx
+++ client/src/pages/Products/LoanCalculator.jsx
@@ -291,13 +291,7 @@ export default function LoanCalculator() {
         )}
       </div>
 
-      {/* <div className="note">
-        <span className="note-icon">i</span>
-        <span className="note-text">
-          <strong>Important:</strong> These are Indicative figures only. Final approval
-          rests with <b>Golden Generation DT SACCO</b>.
-        </span>
-      </div> */}
+    
 
       <Footer />
     </div>
