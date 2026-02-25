d694b415929b436a31efb7bd5f5548c3962c521e
Clive Moyia <maderumoyia@gmail.com>
2026-02-25 12:50:25 +0300

    homepage slider styled for optimization

--- client/src/components/HomepageSlider.jsx
+++ client/src/components/HomepageSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Slider from 'react-slick';
 import { motion, AnimatePresence } from 'framer-motion';
